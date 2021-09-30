1b2fd4bb26fd6608c483d1872c734721e752ad8e
Pedro Chagas <87217922+pChagas-cloud@users.noreply.github.com>
2021-09-30 16:34:14 -0300

    Add files via upload
    Esse é o jogo que eu vou utilizar para fazer a demontração do que foi apresentado

---
+++ jogo_adivinhacao_simples.py
@@ -0,0 +1,29 @@
+import random
+print("Jogo de adivinhação!")
+
+numero_secreto = random.randrange(1,10)
+total_de_tentativas = 3
+rodada = 1
+
+while (rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou " , chute_str)
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Parabéns! Você acertou!")
+    else:
+        if(maior):
+            print("O seu chute foi maior do que o número secreto!")
+        elif(menor):
+            print("O seu chute foi menor do que o número secreto!")
+
+    rodada = rodada + 1
+
+print("Fim do jogo")
